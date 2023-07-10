c33f6611937300e68835057e9992eb52b405b79e
soong-e <yujelee@student.42seoul.kr>
2023-07-10 16:48:12 +0900

    feat: validation 추가

--- src/auth/dto/auth-credential.dto.ts
+++ src/auth/dto/auth-credential.dto.ts
@@ -1,4 +1,18 @@
+import { IsString, Matches, MaxLength, MinLength } from "class-validator";
+
 export class AuthCredentialDto{
+	@IsString()
+	@MinLength(4)
+	@MaxLength(20)
 	username : string;
+
+	@IsString()
+	@MinLength(4)
+	@MaxLength(20)
+	@Matches(/^[a-zA-Z0-9]*$/,{
+		message: 'password only accepts english, number '
+	})
 	password : string;
+
+
 }
