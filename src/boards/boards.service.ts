073033c2dfe931743c7085a7b6f473bffa74e947
soong-e <yujelee@student.42seoul.kr>
2023-07-11 17:26:47 +0900

    feat: 작성자만 게시글 삭제할 수 있게 수정

--- src/boards/boards.service.ts
+++ src/boards/boards.service.ts
@@ -21,7 +21,7 @@ export class BoardsService {
 		query.where('board.userId = :userId', {userId: user.id});
 
 		const boards = await query.getMany();
-		
+
 		return boards;
 	}
 	
@@ -38,8 +38,8 @@ export class BoardsService {
 		return found;
 	}
 
-	async deleteBoard(id: number): Promise<void> {
-		const result = await this.boardRepository.delete(id);
+	async deleteBoard(id: number, user: User): Promise<void> {
+		const result = await this.boardRepository.delete({id, user: {id: user.id}});
 
 		if (result.affected === 0){
 			throw new NotFoundException(`Can't find Board with ID ${id}`);
